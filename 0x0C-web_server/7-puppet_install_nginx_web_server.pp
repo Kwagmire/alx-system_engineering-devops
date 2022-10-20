1a1266935ea0cafbdfac0b1353bf16a1500e5908
Kwagmire <kwagmire999@gmail.com>
2022-10-20 01:55:53 +0100

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,15 @@
+# installs nginx and configure
+
+package { 'nginx':
+  ensure => latest
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!'
+}
+
+exec { 'edit config file':
+  provider => shell,
+  command  => 'sudo sed -i "s/server_name _;/server_name_;\n\trewrite ^\/redirect_me https:\/\/alxafrica.com permanent;/" /etc/nginx/sites-available/default ; sudo service nginx restart'
+}
